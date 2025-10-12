8fd908a3d49642be4ae217ffb0414a7292120b2d
joshardals <joshuabamidele219@gmail.com>
2025-10-12 06:27:43 +0100

    almomst done

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -12,7 +12,7 @@ export default function AuthLayout({
 }) {
   return (
     <div className="flex min-h-screen flex-col bg-white md:bg-neutral-100">
-      <main className="max-w-[1440px] mx-auto w-full mb-12 md:mb-28">
+      <main className="max-w-[1440px] mx-auto w-full mb-28">
         <div className="bg-white mx-4 mt-4 md:pt-4 md:pb-12 md:border md:border-neutral-300 md:rounded-[30px]">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8">
             <div className="pl-4 relative hidden md:block">
