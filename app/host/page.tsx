b36dd9759675dd4977077a68842584726ec3c146
joshardals <joshuabamidele219@gmail.com>
2025-11-03 15:37:40 +0100

    added more things to the my activity component

--- app/host/page.tsx
+++ app/host/page.tsx
@@ -29,14 +29,14 @@ export default function HostHomePage() {
 
         <div className="space-y-1">
           <h1 className="font-gilroy-bold text-4xl md:text-5xl">Welcome</h1>
-          <span className="font-gilroy-medium">User host name</span>
+          <span className="font-gilroy-medium">Joshua Bamidele</span>
         </div>
       </section>
 
-      <div className="bg-neutral-150 h-[1px] w-full mt-3 md:hidden" />
+      <div className="bg-neutral-150 h-[1px] w-full mt-4 md:hidden" />
 
       {/* My Earnings Section */}
-      <section className="mt-3 md:mt-6">
+      <section className="mt-4 md:mt-6">
         <h2 className="font-gilroy-bold text-sm  md:hidden">My Earnings</h2>
         <div className="rounded-[20px] mt-2 p-4 bg-white border border-neutral-150 shadow-[0_6px_17.9px_0_rgba(0,0,0,0.1)]">
           <div className="flex justify-between items-center">
@@ -71,6 +71,41 @@ export default function HostHomePage() {
               >
                 Create Wallet
               </Button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-6">
+        <h2 className="font-gilroy-bold text-sm px-4">My Activity</h2>
+
+        <div className="grid grid-cols-2 md:grid-cols-4 mt-2">
+          <div className="bg-white px-4 pt-4 pb-2 rounded-[16.5px] border border-neutral-75 shadow-[0_6px_18.61px_0_rgba(0,0,0,0.07)]">
+            <div className="flex md:items-center space-x-2 md:space-x-4">
+              <div className="relative size-6 md:size-[38px]">
+                <Image
+                  src={`/images/icons/profile-2user-dark.svg`}
+                  alt="User 2 Profile Icon"
+                  fill
+                  className="object-contain"
+                />
+              </div>
+
+              <div className="flex flex-col">
+                <span className="font-gilroy-bold text-sm md:text-3xl text-primary-dark">
+                  0
+                </span>
+
+                <span className="md:hidden font-gilroy-medium text-xs  text-black-transparent">
+                  Rating
+                </span>
+              </div>
+            </div>
+
+            <div className="hidden md:flex justify-end -mt-1">
+              <span className="font-gilroy-medium text-sm md:text-base text-black-transparent">
+                Rating
+              </span>
             </div>
           </div>
         </div>
---
+++ components/host/dashboard/ActivityCard.tsx
@@ -0,0 +1,3 @@
+export function ActivityCard() {
+  return <div>ActivityCard</div>;
+}
