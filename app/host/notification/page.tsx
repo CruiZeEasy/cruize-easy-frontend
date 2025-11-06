f0a8a8aee52259cae0811b8d02041ae88f83d316
joshardals <joshuabamidele219@gmail.com>
2025-11-06 16:36:16 +0100

    trying to squash some bugs

--- app/host/notification/page.tsx
+++ app/host/notification/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function HostNotificationPage() {
   return (
-    <div className="pb-28">
+    <div className="">
       <HostHeader />
 
       <section>
