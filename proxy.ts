9b1bc1fac531f610852e227490143bd76c205a20
joshardals <joshuabamidele219@gmail.com>
2025-11-08 15:43:10 +0100

    working on the allow-location

--- proxy.ts
+++ proxy.ts
@@ -8,7 +8,7 @@ export function proxy(request: NextRequest) {
   // Define protected routes
   const protectedRoutes = [
     PATHS.ONBOARDING.COMPLETE_PROFILE,
-    PATHS.ONBOARDING.ALLOW_LOCATION,
+    // PATHS.ONBOARDING.ALLOW_LOCATION,
     PATHS.ONBOARDING.ALLOW_NOTIFICATIONS,
     // add more like "/dashboard", "/profile", etc. if needed
   ];
