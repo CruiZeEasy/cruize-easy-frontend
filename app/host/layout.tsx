6c5379d349cd806602fa6ca308a13c7c6aa4ebc5
joshardals <joshuabamidele219@gmail.com>
2025-11-09 06:04:45 +0100

    testing

--- app/host/layout.tsx
+++ app/host/layout.tsx
@@ -6,7 +6,7 @@ export default function HostLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex  bg-neutral-100">
+    <div className="flex md:h-screen bg-neutral-100">
       {/* Sidebar */}
 
       <HostSidebar />
