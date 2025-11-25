83e74d94a3c83111eff4029faa48e273436db75f
joshardals <joshuabamidele219@gmail.com>
2025-11-25 04:17:41 +0100

    updated theh font size of the label

--- components/shared/FilterModal.tsx
+++ components/shared/FilterModal.tsx
@@ -45,7 +45,7 @@ export function FilterModal({ isOpen, onClose }: FilterModalProps) {
               </span>
             </div>
 
-            <div className="overflow-y-auto max-h-[calc(85dvh-140px)] space-y-6">
+            <div className="overflow-y-auto max-h-[calc(85dvh-140px)] space-y-6 text-sm">
               <div>
                 <span>Type</span>
 
